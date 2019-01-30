72a41f56cb9dd1285c5c60b870d6a2af240219cb
Dylan Wong <47100551+Wong-Innovations@users.noreply.github.com>
2019-01-29 16:30:22 -0800

    Fixed indentations

--- project1.cpp
+++ project1.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include <fstream>
 #define LENGTH 10
+#define MAXSIZE 9
 
-void alphabetize(char arr[][255], int pos[]);
-void lenSort(char arr[][255], int pos[]);
-void logNames(std::ofstream& outFile, char arr[][255], int pos[]);
+void alphabetize(char name_list[][MAXSIZE], int pos[]);
+void lengthSort(char name_list[][MAXSIZE], int pos[]);
+void logNames(std::ofstream& outFile, char name_list[][MAXSIZE], int pos[]);
 int myStringCompare(const char str1 [], const char str2 []);
 void myStringCopy(char destination [], const char source []);
 int myStringLength(const char str []);
-void printNames(char arr[][255], int pos[]);
+void printNames(char name_list[][MAXSIZE], int pos[]);
+void swapNames(char name_list[][MAXSIZE], int pos[], const int a, const int b);
 
 int main(void)
 {
-    char source_file_name[255], destination_file_name[255];
-    char name_list[LENGTH][255];
+    char source_file_name[MAXSIZE], destination_file_name[MAXSIZE];
+    char name_list[LENGTH][MAXSIZE];
     int pos[LENGTH] = {0,1,2,3,4,5,6,7,8,9};
 
     std::cout << "Enter source file name: ";
@@ -36,7 +38,7 @@ int main(void)
     std::cout << "=============================" << std::endl;
     printNames(name_list, pos);
 
-    lenSort(name_list, pos);
+    lengthSort(name_list, pos);
 
     std::cout << "\nSorted-by-Length Data (Original Input Order and Name)" << std::endl;
     std::cout << "=============================" << std::endl;
@@ -58,47 +60,35 @@ int main(void)
     return 0;
 }
 
-void alphabetize(char arr[][255], int pos[])
+void alphabetize(char name_list[][MAXSIZE], int pos[])
 {
 	for (int i = 0; i < LENGTH; i++)
     {
         for (int j = 0; j < LENGTH - 1; j++)
         {
-            if (myStringCompare(arr[j], arr[j+1]) == 1)
+            if (myStringCompare(name_list[j], name_list[j+1]) == 1)
             {
-                char temp[255];
-				int storage = pos[j+1];
-                myStringCopy(temp, arr[j+1]);
-                myStringCopy(arr[j+1], arr[j]);
-                myStringCopy(arr[j], temp);
-				pos[j+1] = pos[j];
-				pos[j] = storage;
+                swapNames(name_list, pos, j, j+1);
             }
         }
     }
 }
 
-void lenSort(char arr[][255], int pos[])
+void lengthSort(char name_list[][MAXSIZE], int pos[])
 {
     for (int i = 0; i < LENGTH; i++)
     {
         for (int j = 0; j < LENGTH - i - 1; j++)
         {
-            if (myStringLength(arr[j]) > myStringLength(arr[j+1]))
+            if (myStringLength(name_list[j]) > myStringLength(name_list[j+1]))
             {
-                char temp[255];
-				int storage = pos[j+1];
-                myStringCopy(temp, arr[j+1]);
-                myStringCopy(arr[j+1], arr[j]);
-                myStringCopy(arr[j], temp);
-				pos[j+1] = pos[j];
-				pos[j] = storage;
+                swapNames(name_list, pos, j, j+1);
             }
         }
     }
 }
 
-void logNames(std::ofstream& outFile, char name_list[][255], int pos[])
+void logNames(std::ofstream& outFile, char name_list[][MAXSIZE], int pos[])
 {
     for (int i = 0; i < LENGTH; i++)
     {
@@ -141,10 +131,21 @@ int myStringLength(const char str [])
     return len;
 }
 
-void printNames(char name_list[][255], int pos[])
+void printNames(char name_list[][MAXSIZE], int pos[])
 {
     for (int i = 0; i < LENGTH; i++)
     {
         std::cout << pos[i] << ' ' << name_list[i] << std::endl;
     }
+}
+
+void swapNames(char name_list[][MAXSIZE], int pos[], const int a, const int b)
+{
+    char temp[MAXSIZE];
+int storage = pos[b];
+    myStringCopy(temp, name_list[b]);
+    myStringCopy(name_list[b], name_list[a]);
+    myStringCopy(name_list[a], temp);
+	pos[b] = pos[a];
+	pos[a] = storage;
 }
