86aec8a48f5de77f47cc503c48ade698af3809ab
Wong-Innovations <dylanwong@nevada.unr.edu>
2019-02-14 16:39:08 -0800

    restructured repo

---
+++ project4/project4.cpp
@@ -0,0 +1,8 @@
+/*********************************************************
+ * Name: proj4.c                                         *
+ * Purpose: Calculate best valued rental car.            *
+ * Author: Dylan Wong                                    *
+ * Class: CS 202.1001                                    *
+ *********************************************************/
+#include <iostream>
+#include <fstream>
