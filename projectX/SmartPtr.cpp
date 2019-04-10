6108cdf0708c953416f278c003853bccb00a306b
Wong-Innovations <dylanwong@nevada.unr.edu>
2019-04-10 09:14:25 -0700

    projectX driver code

--- projectX/SmartPtr.cpp
+++ projectX/SmartPtr.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 #include "DataType.h"
 #include "SmartPtr.h"
 
@@ -9,7 +8,8 @@ SmartPtr::SmartPtr( )
     {
         m_ptr = new DataType;
         m_refcount = new size_t;
-        *m_refcount = 1;
+        *m_refcount = 0;
+        if (m_ptr != NULL) m_refcount++;
     }
     catch(const std::bad_alloc & e)
     {
@@ -24,41 +24,56 @@ SmartPtr::SmartPtr( DataType * data )
     m_ptr = data;
     try
     {
-        m_refcount = 0;
-        if (m_ptr) m_refcount++;
+        m_refcount = new size_t;
+        *m_refcount = 0;
+        if (m_ptr != NULL) m_refcount++; // Checks data wasn't NULL
     }
     catch(const std::bad_alloc& e)
     {
         std::cerr << e.what() << '\n';
     }
-    
-    
+    std::cout << "SmartPtr Parametrized Constructor from data pointer, RefCount="
+        << *m_refcount << std::endl;   
 }
 
 SmartPtr::SmartPtr( const SmartPtr & other )
 {
+    m_ptr = other.m_ptr;
+    m_refcount = other.m_refcount;
+    m_refcount++;
     
+    std::cout << "SmartPtr Copy Constructor, RefCount=" << *m_refcount << std::endl;
 }
 
 
 SmartPtr::~SmartPtr( )
 {
-    
+    --(*m_refcount);
+    if (!(*m_refcount)) {
+        delete m_ptr;
+        m_ptr = NULL;
+    }
+
+    std::cout << "SmartPtr Destrcutor, RefCount=" << *m_refcount << std::endl;
 }
 
 
 SmartPtr & SmartPtr::operator=( const SmartPtr & rhs )
 {
-    
+    --(*m_refcount); // Decrease this->count
+    ++(*rhs.m_refcount); // Increase new->count
+
+    if (!(*m_refcount)) {
+        delete m_ptr;
+        m_ptr = NULL;
+    }
+
+    m_ptr = rhs.m_ptr;
+    m_refcount = rhs.m_refcount;
+
+    return (*this);
 }
 
 
-DataType & SmartPtr::operator*( )
-{
-    
-}
-
-DataType * SmartPtr::operator->( )
-{
-    
-}
+DataType & SmartPtr::operator*( ) { return (*this->m_ptr); }
+DataType * SmartPtr::operator->( ) { return this->m_ptr; }
